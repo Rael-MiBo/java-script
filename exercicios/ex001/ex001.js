7d28cf750d4ee2c80ef2f54fa2c912ab6a9a3e20
Rael <121963878+Rael-MiBo@users.noreply.github.com>
2023-01-24 19:36:02 -0300

    a

---
+++ exercicios/ex001/ex001.js
@@ -0,0 +1,27 @@
+console.log ('Olá Mundo!')
+
+var org = '5'
+if (org == 'cv') {
+console.log ('O brabo das putaria')
+} 
+ else if (org == 'pcc') {
+    console.log ('Sai daqui maluko, vmtnc')
+} 
+else {
+    console.log ('Que porra de fac é essa?')
+}
+
+console.log ({
+    name: 'rafael',
+    idade: 17,
+    andar: function() {
+        console.log ('sim')
+    }
+})
+
+console.log ([
+    'rafael',
+    'ovo',
+    12
+
+])
